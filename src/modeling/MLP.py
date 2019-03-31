b19f4c793a12e845ed356e7992aa18bf937a1a03
Julien <julien.delaunay@usherbrooke.ca>
2019-03-31 19:40:20 -0400

    regression logistique, naive bayes completed

--- src/modeling/MLP.py
+++ src/modeling/MLP.py
@@ -1,13 +1,20 @@
 from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
 
 class MLP:
     
     def  __init__(self):
         
         #MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
-		self.parameters = {'hidden_layer_sizes':[(100,)], 'activation':['logistic', 'relu'], 'solver':['sgd'], 
-                                           'alpha':['0.0001'], 'max_iter':['200']}
-		self.model = MLPClassifier(self.parameters)
+		self.model = SVC()
+        self.parameters = {'hidden_layer_sizes':[(100,)], 'activation':['logistic', 'relu'], 'solver':['sgd'],
+                                                 'alpha':['0.0001'], 'max_iter':['200']}        
+        self.CV = 5
+		self.clf = GridSearchCV(self.model, self.parameters,
+									n_jobs = 4,
+									cv = self.CV)
+
+
 
     def train(self, X_train, t_train):
 
