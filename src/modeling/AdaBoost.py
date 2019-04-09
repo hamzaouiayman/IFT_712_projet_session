9c5b8b60dea13a69a39548eec7e70ea2a9052f4c
Aiman Hamzaoui <hama2109@dinf-0014-41b.DInf.fsci.usherbrooke.ca>
2019-04-09 14:40:46 -0400

    test adaBoost

--- src/modeling/AdaBoost.py
+++ src/modeling/AdaBoost.py
@@ -1,37 +1,41 @@
 from sklearn.ensemble import AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
 
+
 class AdaBoost:
-    
-    def  __init__(self,grid_search):
-        
-        self.grid_search = grid_search
-        self.model = LogisticRegression(C=5, class_weight=None, fit_intercept=True, multi_class='multinomial',solver='sag',tol=1e-05)
-        self.estimators = 5
-        self.clf = AdaBoostClassifier(self.model,algorithm="SAMME",
-                         				n_estimators=200)
 
-    def train(self, X_train, t_train):
+	def __init__(self, grid_search):
 
-    	if (self.grid_search == True):
-    		self.clf.fit(X_train.toarray(), t_train)
-    	else:
-    		self.model.fit(X_train.toarray(), t_train)
+		self.grid_search = grid_search
+		self.model = LogisticRegression(C=5, class_weight=None, fit_intercept=True, multi_class='multinomial',
+										solver='sag', tol=1e-05)
+		self.estimators = 5
+		self.clf = AdaBoostClassifier(self.model, n_estimators=3)
 
-    def predict(self, X_test):
+	def train(self, X_train, t_train):
 
-    	if (self.grid_search == True):
-    		y_predict = self.clf.predict(X_test.toarray())
-    	else:
-    		y_predict = self.model.predict(X_test.toarray())
+		if (self.grid_search == True):
+			self.clf.fit(X_train.toarray(), t_train)
+		else:
+			self.model.fit(X_train.toarray(), t_train)
 
-    	return y_predict
+	def predict(self, X_test):
 
-    def score(self, X_train, t_train):
+		if (self.grid_search == True):
+			y_predict = self.clf.predict(X_test.toarray())
+		else:
+			y_predict = self.model.predict(X_test.toarray())
 
-    	if (self.grid_search == True):
-    		score = self.clf.score(X_train.toarray(), t_train)
-    	else:
-    		score = self.model.score(X_train.toarray(), t_train)
+		return y_predict
 
-    	return score
+	def score(self, X_train, t_train):
+
+		if (self.grid_search == True):
+			score = self.clf.score(X_train.toarray(), t_train)
+		else:
+			score = self.model.score(X_train.toarray(), t_train)
+
+		return score
+
+	def print_cv_results(self):
+			return
